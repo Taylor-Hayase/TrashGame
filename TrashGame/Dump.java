cef095db3d5d471ddd07e86f27a816368799433f
Taylor Hayase <thayase@calpoly.edu>
2020-02-04 16:47:46 -0800

    poop

--- TrashGame/Dump.java
+++ TrashGame/Dump.java
@@ -16,7 +16,7 @@ public class Dump extends World
     public Dump()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(1200, 1200, 1);
+        super(1200, 700, 1);
         GreenfootImage img = new GreenfootImage("dump.jpg");
         setBackground(img);
         
